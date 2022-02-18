a12d042e679ce7165727b1f8221006a9ab8ee22e
jgsjss9854 <87510168+jgsjss@users.noreply.github.com>
2022-02-18 15:01:41 +0900

    qwe

--- routes/menu.js
+++ routes/menu.js
@@ -6,10 +6,15 @@ const {urlencoded, request} = require("express");
 
 
 //카테고리 페이지 첫화면
-router.post('/categories', async function (req, res) {
-    let [rows, fields] = await db.query("select * from categories")
+router.post('/categories', async function (req , res) {
+
+    //조인문 쿼리
+    let joinquery =  "select * from categories as a right outer join subcategories b on a.ctnum = b.ctnum"
+
+    //카테고리와 서브카테고리 조인문
+    let [rows, joinfields] = await db.query(joinquery);
+
     console.log(rows)
-    console.log(fields)
     res.status(200).json(rows)
 })
 
