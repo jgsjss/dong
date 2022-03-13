d58247f18932e0a10a455d373649c8a0e7502946
jks123 <jgsjss9854@naver.com>
2022-03-14 08:47:36 +0900

    qwe
    qwe

--- routes/menu.js
+++ routes/menu.js
@@ -119,11 +119,12 @@ router.post('/menus', async function (req, res){
 
 router.post("/pdupload", upload.single("image"),  (req,res) => {
     try{
-        sharp(req.file.path)
-            .resize({ width: 300 })
-            .withMetadata()
+        sharp(req.file.path) //압축할 이미지 경로
+            .resize({ width: 300 }) //비율을 유지하며 가로 크기 줄이기(반응형)
+            .withMetadata()  //이미지의 exif 데이터 유지
             .toBuffer((err, buffer) => {
                 if(err) throw err;
+                //압축된 파일 새로 저장(덮어씌우기)
                 fs.writeFile(req.file.path, buffer, (err) =>{
                     if(err) throw err;
                 })
