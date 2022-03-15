7d26280314e791559d64573b68844aa0acbba76a
liam <eetadev01@gmail.com>
2022-03-15 13:34:53 +0900

    Update menu.js

--- routes/menu.js
+++ routes/menu.js
@@ -4,16 +4,16 @@ const db = require('../db/mysql')
 const path = require("path")
 const {urlencoded, request} = require("express");
 const _ = require("lodash");
-const { pdupload } = require("../config/pdupload")
+const {pdupload} = require("../config/pdupload")
 const sharp = require("sharp")
 const fs = require("fs")
 
 
-router.post('/menucategory', async function(req, res){
+router.post('/menucategory', async function (req, res) {
     let [rows, fields] = await db.query('select * from paycoq.categories')
     console.log(rows)
 
-    res.status(200).json({rows : rows})
+    res.status(200).json({rows: rows})
 })
 
 
@@ -47,7 +47,6 @@ router.post('/categories', async function (req, res) {
     let articleLengthQuery = "select * from categories as a right outer join products b on a.ctnum = b.ctnum order by b.pdnum asc"
 
 
-
     let [articleLengthBefore] = await db.query(articleLengthQuery)
     let ActualArticleLength = Math.ceil(articleLengthBefore.length / 10)
     console.log("목록 리스트 갯수 : ", ActualArticleLength)
@@ -55,7 +54,7 @@ router.post('/categories', async function (req, res) {
     //카테고리와 서브카테고리 조인문
     let [rows, joinfields] = await db.query(joinquery, result);
 
-    let articles = {rows:rows, length:ActualArticleLength}
+    let articles = {rows: rows, length: ActualArticleLength}
     // console.log(rows)
     res.status(200).json(articles)
 })
@@ -77,7 +76,7 @@ router.post('/addcategory', async function (req, res) {
 
 })
 
-router.post('/menus', async function (req, res){
+router.post('/menus', async function (req, res) {
     let curpage = req.body.data.curpage
     let pageSize = 5;
 
@@ -105,7 +104,6 @@ router.post('/menus', async function (req, res){
     let articleLengthQuery = "select * from categories as a right outer join products b on a.ctnum = b.ctnum order by b.pdnum asc"
 
 
-
     let [articleLengthBefore] = await db.query(articleLengthQuery)
     let ActualArticleLength = Math.ceil(articleLengthBefore.length / 5)
     console.log("목록 리스트 갯수 : ", ActualArticleLength)
@@ -113,37 +111,38 @@ router.post('/menus', async function (req, res){
     //카테고리와 프로덕츠 조인문
     let [rows, joinfields] = await db.query(joinquery, result);
 
-    let articles = {rows:rows, length:ActualArticleLength}
+    let articles = {rows: rows, length: ActualArticleLength}
     // console.log(rows)
     res.status(200).json(articles)
 })
 
-router.post("/pdupload", pdupload.single("image"),  (req,res) => {
-    try{
+router.post("/pdupload", pdupload.single("image"), (req, res) => {
+    try {
         sharp(req.file.path) //압축할 이미지 경로
-            .resize({ width: 300 }) //비율을 유지하며 가로 크기 줄이기(반응형)
+            .resize({width: 300}) //비율을 유지하며 가로 크기 줄이기(반응형)
             .withMetadata()  //이미지의 exif 데이터 유지
             .toBuffer((err, buffer) => {
-                if(err) throw err;
+                if (err) throw err;
                 //압축된 파일 새로 저장(덮어씌우기)
-                fs.writeFile(req.file.path, buffer, (err) =>{
-                    if(err) throw err;
+                fs.writeFile(req.file.path, buffer, (err) => {
+                    if (err) throw err;
                 })
             })
-    }catch (err){
+    } catch (err) {
         console.log(err)
     }
-    res.json({ filename: `${req.file.filename}`});
+    res.json({filename: `${req.file.filename}`});
 })
 
-router.post("/addMenu", async function (req, res){
+router.post("/addMenu", async function (req, res) {
     let products = new Array()
-    _.map(req.body, (value, key, collection) => {
+    _.map(req.body.data, (value, key, collection) => {
         products.push(value)
         console.log("키", key)
         console.log("벨류", value)
         console.log("콜렉션", collection)
     })
+
     let insertquery = "insert into paycoq.products value(null, ?, ?, ?, ?, null, null)"
     let [rows, fields] = await db.query(insertquery, products)
 
