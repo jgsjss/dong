990cd33603eb33e4ce847676b6ffea4a36094f21
liam <eetadev01@gmail.com>
2022-03-16 11:09:44 +0900

    static
    http://localhost:5000/static/pdimage/dobi.jpg

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/static', express.static('uploads'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
